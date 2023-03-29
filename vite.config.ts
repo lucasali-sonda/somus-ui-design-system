15edc8e1d9dab627831b7a95e4659015ae7b7503
lucasali-sonda <lucas.sbardelotto@sonda.com>
2023-03-28 21:16:39 -0300

    🔧 chore(vite.config.ts): simplify plugins array by using Vue plugin instead of importing and using vue() and dts() separately 🚀 feat(vite.config.ts): add outDir option to build configuration 🔧 chore(vite.config.ts): remove unused options from typescript2 plugin configuration 🔧 chore(vite.config.ts): remove input option from rollupOptions since it's not needed 🔧 chore(vite.config.ts): remove assetFileNames and exports options from output configuration since they are not needed 🔧 chore(vite.config.ts): remove resolve configuration since it's not needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-
-import dts from 'vite-plugin-dts'
-import typescript2 from 'rollup-plugin-typescript2'
-
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		dts({
-			insertTypesEntry: true
-		}),
-		typescript2({
-			check: false,
-			include: ['src/components/**/*.vue'],
-			tsconfigOverride: {
-				compilerOptions: {
-					outDir: 'dist',
-					sourceMap: true,
-					declaration: true,
-					declarationMap: true
-				}
-			},
-			exclude: ['vite.config.ts']
-		})
-	],
+	plugins: [Vue({})],
 	build: {
-		cssCodeSplit: true,
+		outDir: './dist',
 		lib: {
-			entry: 'src/components/main.ts',
+			entry: resolve(__dirname, 'src/index.ts'),
 			name: 'SomusUIDesignSystem',
 			fileName: (format) => `somus-ui-design-system.${format}.js`
 		},
 		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'src/components/main.ts')
-			},
 			external: ['vue'],
 			output: {
-				assetFileNames: (assetInfo) => {
-					if (assetInfo.name === 'main.css') return 'somus-ui-design-system.css'
-					return assetInfo.name!
-				},
-				exports: 'named',
 				globals: {
 					vue: 'Vue'
 				}
 			}
-		}
-	},
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
 	}
 })
